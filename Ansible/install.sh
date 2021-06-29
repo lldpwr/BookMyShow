330ccf939aa175fab469c2b79b3577f702a5bede
Lloyd Salvant <lldpwr@gmail.com>
2021-06-29 04:58:00 +0000

    remove ssh key generation from the ansible install

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -1,9 +1,13 @@
 
-!#/bin/sh
+#!/bin/sh
 # Download and install the prerequisite
 sudo apt update && sudo apt install -y software-properties-common python3-pip
 # Install Ansible
 sudo apt install -y ansible
 # Verify current ansible version
 ansible --version
+# Copy the ssh key to authorise the connection to this machine
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+# Set the host to this machine, ie localhost
+cat Ansible/hosts | sudo tee -a /etc/ansible/hosts
 
