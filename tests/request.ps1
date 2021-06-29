fe2a660fa923ecde47dc5d32dc8131f06cdbfffc
Lloyd <lloyd@matrix.ca>
2021-06-29 05:17:05 -0400

    it seems that jenkins can not proecess the output

--- tests/request.ps1
+++ tests/request.ps1
@@ -1,6 +1,6 @@
 $ip = (hostname -I) -split " " |  Select-Object -First 1
 Write-Host "Ip address $ip"
-Invoke-WebRequest -Uri http://${ip}:8086/pay
-Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'item': 12345 }"  -ContentType "application/json"
-Invoke-WebRequest -Uri http://${ip}:8086/pay
-Invoke-WebRequest -Uri http://${ip}:8086/shutdown
+Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'item': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
+Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
