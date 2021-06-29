86a7db63a19e49d58272117f7a092d5e4f6883c0
Lloyd <lloyd@matrix.ca>
2021-06-29 05:42:00 -0400

    try string for the output of powershell

--- tests/request.ps1
+++ tests/request.ps1
@@ -1,6 +1,7 @@
 $ip = (hostname -I) -split " " |  Select-Object -First 1
 Write-Host "Ip address $ip"
-Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+$content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+$content.ToString()
 Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'item': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
 Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
 Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
