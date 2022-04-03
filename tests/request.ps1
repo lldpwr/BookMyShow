87fb29dcdd8b1b84f9d37d3d82c5dcd75812b378
Lloyd Salvant <lsalvant@outlook.com>
2022-04-03 21:12:45 +0000

    cherry pick installJenkins.yaml request.ps1 and also inlcude install in request

--- tests/request.ps1
+++ tests/request.ps1
@@ -1,8 +1,17 @@
-$ip = (hostname -I) -split " " |  Select-Object -First 1
-Write-Host "Ip address $ip"
-$content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
-$content -match "succeed"
-$content = Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'index': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
-$content -match "12345"
-$content = Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
-$content -match "Thank you"
+Install-Module -Name Pode -Confirm:$false -force -Scope User
+Install-Module -Name Pode.Kestrel -Confirm:$false -force -Scope User
+
+Start-Job { 
+	Wait-Event -SourceIdentifier "" -Timeout 10;
+	$ip = (hostname -I) -split " " |  Select-Object -First 1
+	Write-Host "Ip address $ip"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+	$content -match "succeed"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'index': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
+	$content -match "12345"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
+	$content -match "Thank you"
+};
+
+./startserver.ps1
+Get-Job | Receive-Job
