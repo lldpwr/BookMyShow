fe6780e2e52bd1ed1215a239ee877bd54e7fd265
Lloyd Salvant <lsalvant@outlook.com>
2022-04-03 18:40:12 +0000

    fix cannot load pode in module job

--- tests/request.ps1
+++ tests/request.ps1
@@ -1,11 +1,14 @@
-Start-Job { ./startserver.ps1};
-Wait-Event -SourceIdentifier "" -Timeout 10;
-$ip = (hostname -I) -split " " |  Select-Object -First 1
-Write-Host "Ip address $ip"
+Start-Job { 
+	Wait-Event -SourceIdentifier "" -Timeout 10;
+	$ip = (hostname -I) -split " " |  Select-Object -First 1
+	Write-Host "Ip address $ip"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
+	$content -match "succeed"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'index': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
+	$content -match "12345"
+	$content = Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
+	$content -match "Thank you"
+};
+
+./startserver.ps1
 Get-Job | Receive-Job
-$content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
-$content -match "succeed"
-$content = Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'index': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
-$content -match "12345"
-$content = Invoke-WebRequest -Uri http://${ip}:8086/shutdown | Select-Object -ExpandProperty RawContent
-$content -match "Thank you"
