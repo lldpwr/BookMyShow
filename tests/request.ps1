cab611e467bcede6f0c8fba74a4f2e216ebf48c5
Lloyd Salvant <lsalvant@outlook.com>
2022-04-03 18:07:03 +0000

    Try to display running job content

--- tests/request.ps1
+++ tests/request.ps1
@@ -2,6 +2,7 @@ Start-Job { ./startserver.ps1};
 Wait-Event -SourceIdentifier "" -Timeout 25;
 $ip = (hostname -I) -split " " |  Select-Object -First 1
 Write-Host "Ip address $ip"
+Get-Job | Receive-Job
 $content = Invoke-WebRequest -Uri http://${ip}:8086/pay | Select-Object -ExpandProperty RawContent
 $content -match "succeed"
 $content = Invoke-WebRequest -Uri http://${ip}:8086/Selectitem -Method Post -Body "{ 'index': 12345 }"  -ContentType "application/json" | Select-Object -ExpandProperty RawContent
